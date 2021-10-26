0f646e7994959255e5750384c1192795bcc3d1ca
Poiki <viserion1981@gmail.com>
2021-10-26 19:00:34 +0200

    css cambiado

--- views/header.php
+++ views/header.php
@@ -3,16 +3,19 @@
 
 <head>
     <title>Gestor de Reservas</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link href="assets/bootstrap.css" rel="stylesheet">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <header class="modal-header">
-        <ul class="navbar">
-            <li><a href="index.php" class="navbar-text">Inicio</a></li>
-            <li><a href="index.php?action=showAllResources&controller=controllerResource" class="navbar-text">Recursos</a></li>
-            <li><a href="index.php?action=showAllTimeSlots&controller=controllerTimeSlot" class="navbar-text">Horario</a></li>
-            <li><a href="index.php?action=showAllUsers&controller=controllerUser" class="navbar-text">Usuarios</a></li>
+        <ul>
+            < class="navbar" style="list-style:none;">
+                <li><a href="index.php" class="navbar-text" style="padding-right: 2em;">Inicio</a></li>
+                <li><a href="index.php?action=showAllResources&controller=controllerResource" class="navbar-text" style="padding-right: 2em;">Recursos</a></li>
+                <li><a href="index.php?action=showAllTimeSlots&controller=controllerTimeSlot" class="navbar-text" style="padding-right: 2em;">Horario</a></li>
+                <li><a href="index.php?action=showAllUsers&controller=controllerUser" class="navbar-text" style="padding-right: 2em;">Usuarios</a></li>
         </ul>
         <div class="">
             <button type="button" class="">Login</button>
